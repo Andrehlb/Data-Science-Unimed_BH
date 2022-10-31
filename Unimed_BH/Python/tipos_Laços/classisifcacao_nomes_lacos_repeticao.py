a525101968d466a5443e89c4399ec1dde297517f
Andrehlb (Andre Luiz Barbosa) <andrehlbarbosa@gmail.com>
2022-10-30 21:40:46 -0300

    upgrade Data-Science-Unimed_BH

--- Unimed_BH/Python/tipos_Laços/classisifcacao_nomes_lacos_repeticao.py
+++ Unimed_BH/Python/tipos_Laços/classisifcacao_nomes_lacos_repeticao.py
@@ -6,9 +6,9 @@
 # letra = h
 # letra = o
 # letra = n
-
-texto = input('Digite um nome: ')
-
+#um elemento previamente conhecido com estrutura iterável, o for é mais indicado.
+#texto = input('Digite um nome: ')
+texto = ""
 vogais = 'AEIOU'
 print(f'Iterando item por item de {texto}, fica: ')
 for c in texto:
@@ -16,3 +16,14 @@ for c in texto:
 print() #quebra de linha
 
 #For tem mais uma instrução o for..else
+print()
+print('Usando o for..else')
+texto = ''
+vogais = 'AEIOU'
+#print(f'Iterando item por item de {texto}, fica: ')
+for c in texto:
+    print(c + '|', end='')
+
+else:
+    print() #quebra de linha / identação diferente
+    print('Executando no final do laço, após a quebra de linha')
